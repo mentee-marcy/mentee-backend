d2dd1530dd12131164cb54f7002149da9e2c993f
josemazhuvanchery1 <jose1998mathew@gmail.com>
2022-12-06 19:34:36 -0500

    update:get profile route

--- seeds/02_mentor_seed.js
+++ seeds/02_mentor_seed.js
@@ -8,6 +8,8 @@ exports.seed = async function(knex) {
   await knex('mentor').insert([
     {mentor_id: 1, company_name: 'spotify', bio:'hi', title:'software engineer I',location:'hicksville'},
     {mentor_id: 2, company_name: 'google', bio:'hello', title:'software enginner II',location:'levittown'},
-    {mentor_id: 3, company_name: 'boomberg', bio:'hola', title:'senior software engineer',location:'nyc'}
+    {mentor_id: 4, company_name: 'boomberg', bio:'hola', title:'senior software engineer',location:'nyc'},
+    {mentor_id: 5, company_name: 'marcy lab', bio:'lead technical instructor at marcy', title:'software instructor',location:'nyc'},
+    {mentor_id: 6, company_name: 'marcy lab', bio:'associate technical instructor at marcy', title:'software instructor',location:'nyc'}
   ]);
 };
