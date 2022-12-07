36676729b03fecffbe6565c1c0a1def15fb51e25
Shadman <shadmanchowdhury01@gmail.com>
2022-12-07 13:01:24 -0500

    feat

--- migrations/20221126202746_messages.js
+++ migrations/20221126202746_messages.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
     table.integer('user_to').references('id').inTable('users');
     table.integer('user_from').references('id').inTable('users');
     table.string('text');
+    table.timestamps(true, true);
   })
 };
 
