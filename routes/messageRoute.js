d61a0c1b87b7abd28b208bda6878488dc59c7d7a
josemazhuvanchery1 <jose1998mathew@gmail.com>
2022-12-11 21:44:28 -0500

    adding messages routes/controller/model setup

--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -4,5 +4,5 @@ const verifyJWT = require('../auth')
 const messagesControl = require('../controllers/messagesControl')
 
 router.get('/:senderId/:recieverId', messagesControl.getMessages);
-
+router.post('/:senderId/:recieverId',messagesControl.addMessage)
 module.exports = router
